a7ad0218f22f5252b40fafaef674923591b27b57
LAKSHYA SHARMA <152636999+lakshyasharma9@users.noreply.github.com>
2025-06-13 15:48:14 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ export const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
   
   useEffect(() => {
-    fetch('http://localhost:4001/popularinwomen')
+    fetch('https://hip-hop-backend.onrender.com/popularinwomen')
       .then((response) => response.json())
       .then((data) => setPopularProducts(data))
       .catch((error) => console.error("Error fetching popular products:", error));
